4b1f42e343065c96c1a73adc4df461892bedf80d
Ming-Te Chi <mtchi@cs.nccu.edu.tw>
2015-02-13 20:32:21 +0800

    better motion illusion
    Control the width by distance field, not density.
    Todo: histogram equalizarion to balance the color??

--- include/postProcessing.h
+++ include/postProcessing.h
@@ -7,18 +7,20 @@ using namespace std;
 class PP{
 public:
 	PP(Size);
-	void motionIllu(Mat &src,Mat &flowfield,Mat &dis);
-	void dirTexture(Mat &src,Mat &flowfield,Mat &dis); 
-	void dirTexture_Polar(Mat &src, Mat &flowfield, Mat &dis);
-	void adaThresholding(Mat &src,Mat &mask,Mat &dis);
-	void LIC(Mat &flowfield, Mat &dis);
-	void Thresholding(Mat &src, Mat &dis);
-	void Colormapping(Mat &src, Mat &mask, Mat &oriImg, Mat &dis);
-	void ReadTexture(string);
-	Mat texture;
-	float alpha;
-	float beta;
-	bool TextureLoaded;
+	void	motionIllu(Mat &src,Mat &flowfield,Mat &dis);
+	void	dirTexture(Mat &src,Mat &flowfield,Mat &dis); 
+	void	dirTexture_Polar(Mat &src, Mat &flowfield, Mat &dis);
+	void	adaThresholding(Mat &src,Mat &mask,Mat &dis);
+	void	LIC(Mat &flowfield, Mat &dis);
+	void	Thresholding(Mat &src, Mat &dis);
+	void	Colormapping(Mat &src, Mat &mask, Mat &oriImg, Mat &dis);
+	void	ReadTexture(string);
+
+	Mat		texture;
+	float	alpha;
+	float	beta;
+	bool	TextureLoaded;
+
 private:
-	void SeedGradient(Mat &src, Mat &Gradient);
+	void	SeedGradient(Mat &src, Mat &Gradient);
 };
